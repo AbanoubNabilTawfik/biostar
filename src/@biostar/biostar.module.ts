fa39b9cafa285b0c6ca70f507156203d68945a08
Abanoub Nabil <abanoub.nabil2016@gmail.com>
2022-12-03 13:24:33 +0200

    creat folders

---
+++ src/@biostar/biostar.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BiostarRoutingModule } from './biostar-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BiostarRoutingModule
+  ]
+})
+export class BiostarModule { }
